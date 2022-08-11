f4d5b9a2e6cba642131b1bcb8e0c186f9344ab01
Real-Musafir <alshahadath@gmail.com>
2022-08-11 18:00:43 +0600

    create function for vrf2 in my Ruffle contract

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -10,15 +10,21 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.9;
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
-    constructor(uint256 entranceFee) {
+    /* Events */
+    event RaffleEnter(address indexed player);
+
+    constructor(address vrfCoordinatorV2, uint256 entranceFee)
+        VRFConsumerBaseV2(vrfCoordinatorV2)
+    {
         i_entranceFee = entranceFee;
     }
 
@@ -29,9 +35,22 @@ contract Raffle {
             revert Raffle__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
+        // emit an event when we update a dynamic array or mapping
+        // Named events with the function name reversed
+
+        emit RaffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner() {}
+    function requestRandomWinner() external {
+        //Request the random number
+        //Once we get it, do something with it
+        // 2 transaction process
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
