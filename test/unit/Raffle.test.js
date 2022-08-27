a713ffdfb240c201a680e911eeaf79dcbf7b273a
Real-Musafir <alshahadath@gmail.com>
2022-08-27 16:48:24 +0600

    test enter raffle

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -1,4 +1,4 @@
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 const { network, getNamedAccounts, deployments, ethers } = require("hardhat");
 const {
   developmentChains,
@@ -8,18 +8,19 @@ const {
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe("Raffle", async function () {
-      let raffle, vrfCoordinatorV2Mock;
+      let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer;
 
       const chainId = await network.config.chainId;
 
       beforeEach(async function () {
-        const { deployer } = await getNamedAccounts();
+        deployer = (await getNamedAccounts()).deployer;
         await deployments.fixture(["all"]);
         raffle = await ethers.getContract("Raffle", deployer);
         vrfCoordinatorV2Mock = await ethers.getContract(
           "VRFCoordinatorV2Mock",
           deployer
         );
+        raffleEntranceFee = await raffle.getEntranceFee();
       });
 
       describe("constructor", async function () {
@@ -32,6 +33,19 @@ const {
           assert.equal(raffleState.toString(), "0");
 
           assert.equal(interval.toString(), networkConfig[chainId]["interval"]);
+        });
+      });
+
+      describe("enterRaffle", async function () {
+        it("reverts when you don't pay enough", async function () {
+          await expect(raffle.enterRaffle()).to.be.revertedWith(
+            "Raffle__NotEnoughETHEntered"
+          );
+        });
+        it("records players when they entered", async function () {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          const playerFromContract = await raffle.getPlayer(0);
+          assert.equal(playerFromContract, deployer);
         });
       });
     });
