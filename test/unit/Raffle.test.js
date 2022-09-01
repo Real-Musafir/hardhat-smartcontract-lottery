5ea9f39c937d61e45b913dd30750a283c1476b2e
Real-Musafir <alshahadath@gmail.com>
2022-09-01 19:47:28 +0600

    test for emit log

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -47,5 +47,10 @@ const {
           const playerFromContract = await raffle.getPlayer(0);
           assert.equal(playerFromContract, deployer);
         });
+        it("emits event on enter", async function () {
+          await expect(
+            raffle.enterRaffle({ value: raffleEntranceFee })
+          ).to.emit(raffle, "RaffleEnter");
+        });
       });
     });
